c291e31f1f2da816ebd28d2a50c0f0a5a40dd458
Breno <brenobcsilva@gmail.com>
2023-05-02 15:57:28 -0300

    First commit

---
+++ src/main/java/com/example/iniciandospringangular/IniciandoSpringAngularApplication.java
@@ -0,0 +1,13 @@
+package com.example.iniciandospringangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IniciandoSpringAngularApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IniciandoSpringAngularApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/iniciandospringangular/IniciandoSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iniciandospringangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IniciandoSpringAngularApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
